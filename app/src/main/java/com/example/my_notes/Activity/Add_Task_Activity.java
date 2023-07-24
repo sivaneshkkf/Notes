99d808f390f0be14fc230914df4aebe895a9d2de
adithview <aathil444@gmail.com>
2023-07-24 15:14:17 +0530

    adith second commit

--- app/src/main/java/com/example/my_notes/Activity/Add_Task_Activity.java
+++ app/src/main/java/com/example/my_notes/Activity/Add_Task_Activity.java
@@ -2,19 +2,59 @@ package com.example.my_notes.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.my_notes.API.APICallbacks;
+import com.example.my_notes.API.APIStatus;
+import com.example.my_notes.API.APPConstants;
 import com.example.my_notes.R;
+import com.example.my_notes.Utils.NetworkController;
 import com.example.my_notes.databinding.ActivityAddTaskBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Add_Task_Activity extends AppCompatActivity {
 ActivityAddTaskBinding binding;
+    Activity activity;
+    String userId,title,subject,desc;
+    APICallbacks apiCallbacks=new APICallbacks() {
+        @Override
+        public void taskProgress(String tag, int progress, Bundle bundle) {
+
+        }
+
+        @Override
+        public void taskFinish(APIStatus apiStatus, String tag, JSONObject response, String message, Bundle bundle) {
+            try {
+                if(apiStatus==APIStatus.SUCCESS){
+                    if(tag.equalsIgnoreCase("createNotes")){
+                        if(response.getBoolean("status")){
+                            JSONObject object=response.getJSONObject("");
+
+                        }else{
+                            Toast.makeText(activity, "error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddTaskBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
 
         binding.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +62,14 @@ ActivityAddTaskBinding binding;
                 finish();
             }
         });
+    }
+
+    private void callapi() {
+        Map<String, String> map = new HashMap<>();
+        map.put("userid", userId);
+        map.put("Title", title);
+        map.put("Subject", subject);
+        map.put("Desc", desc);
+        NetworkController.getInstance().callApiPost(activity, APPConstants.MAIN_URL + "createNotes", map, "createNotes", new Bundle(), apiCallbacks);
     }
 }
